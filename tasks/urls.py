38d47d90be81c9152797974f7b7b0b5350d4f4c8
rjdrk <rjdrk2.5@gmail.com>
2025-02-26 22:55:32 -0600

    Metodo PUT

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import create_task, list_tasks,get_task
+from .views import create_task, list_tasks, get_task, update_task, delete_task
 
 urlpatterns = [
     path('tasks/create/', create_task, name='create_task'),
     path('tasks/', list_tasks, name='list_tasks'),
-    path("tasks/<int:task_id>/", get_task, name="get-task"),
-    #path('<int:task_id>/update/', views.update_task, name='update_task'),
-    #path('<int:task_id>/delete/', views.delete_task, name='delete_task'),
+    path('tasks/<int:task_id>/', get_task, name='get-task'),
+    path('tasks/update/<int:task_id>/', update_task, name='update_task'), 
+    path('tasks/delete/<int:task_id>/', delete_task, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -39,8 +39,23 @@ def get_task(request, task_id):
 
     try:
         task = Task.objects.get(task_id=task_id)
-        print(task)
         serializer = TaskSerializer(task)
         return Response(serializer.data)
     except Task.DoesNotExist:
         return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+@api_view(['PUT'])
+def update_task(request, task_id):
+    if not validate_api_key(request):
+        return Response({'error': 'Unauthorized'}, status=status.HTTP_401_UNAUTHORIZED)
+
+    try:
+        task = Task.objects.get(task_id=task_id)
+    except Task.DoesNotExist:
+        return Response({'error': 'Task not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = TaskSerializer(task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
